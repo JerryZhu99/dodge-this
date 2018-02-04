51336f2be46cb063b2b89adbc176c5e1327f3c6b
JerryZ <jerryzhu99@gmail.com>
2018-02-03 23:31:24 -0500

    Performance improvements. Removed target line.

--- client/src/projectile-object.ts
+++ client/src/projectile-object.ts
@@ -14,8 +14,8 @@ export default class ProjectileObject extends Projectile {
         this.display = new Container();
         this.trailPoints = new Array<Vector>();
         let sprite = new Sprite(circle);
-        sprite.scale = new Point(10/1024,10/1024);
-        sprite.position = new Point(-5,-5);
+        sprite.scale = new Point(10 / 1024, 10 / 1024);
+        sprite.position = new Point(-5, -5);
         sprite.tint = teamColours[this.team];
         this.display.addChild(sprite);
 
@@ -27,34 +27,11 @@ export default class ProjectileObject extends Projectile {
     }
     update(deltaTime: number) {
         super.update(deltaTime);
-        this.display.position.x = this.position.x * 0.5 + this.display.position.x * 0.5;
-        this.display.position.y = this.position.y * 0.5 + this.display.position.y * 0.5;
-        if(isNaN(this.display.position.y)) this.display.position.y = this.position.y;
-        if(isNaN(this.display.position.x)) this.display.position.x = this.position.x;
-        this.drawLead();
-    }
-
-    drawTrail() {
-        let p = this.position;
-        let g = this.trail;
-        g.clear();
-        g.moveTo(0, 0);
-        for (let i = 0; i < this.trailPoints.length; i++) {
-            let q = this.trailPoints[i];
-            g.lineStyle((10 - i) / 10 * this.radius, teamColours[this.team], (10 - i) / 10);
-            g.lineTo(q.x - p.x, q.y - p.y);
+        if (this.position.distanceTo(this.display.position) > 1) {
+            this.display.position.x = this.position.x * 0.5 + this.display.position.x * 0.5;
+            this.display.position.y = this.position.y * 0.5 + this.display.position.y * 0.5;
         }
-        this.trailPoints.unshift(new Vector(p));
-        if (this.trailPoints.length > 10) this.trailPoints.pop();
-    }
-
-    drawLead() {
-        let p = this.position;
-        let g = this.lead;
-        g.clear();
-        g.lineStyle(2, teamColours[this.team], 0.4);
-        g.moveTo(0, 0);
-        let d = this.velocity.scaled(this.lifeTime - this.time)
-        g.lineTo(d.x, d.y);
+        if (isNaN(this.display.position.y)) this.display.position.y = this.position.y;
+        if (isNaN(this.display.position.x)) this.display.position.x = this.position.x;
     }
 }
--- server/game-manager.ts
+++ server/game-manager.ts
@@ -11,18 +11,14 @@ import * as uuid from "uuid/v4";
  */
 export default class GameManager {
     games: Map<string, ServerState>;
-    private timer: NodeJS.Timer 
+
+    private expected: number; 
+    private updateInterval = 1000 / 60.0;
     constructor(wss: WebSocket.Server) {
 
         this.games = new Map<string, ServerState>();
-        this.timer = setInterval(() => {
-            this.games.forEach((game, key) => {
-                game.update(1 / 60.0);
-                if (game.players.length == 0 || game.time > 1000) {
-                    this.games.delete(key);
-                }
-            });
-        }, 1000 / 60.0);
+        this.expected = Date.now() - this.updateInterval;
+        this.updateGames();
 
         wss.on('connection', (ws, req) => {
             let socket = new SocketWrapper(ws);
@@ -57,6 +53,21 @@ export default class GameManager {
                 gameState.removeConnection(player);
             });
         });
+    }
+
+    public updateGames(){
+        let dt = Date.now() - this.expected;
+        if(dt > this.updateInterval){
+            console.error("server is behind");
+        }
+        this.games.forEach((game, key) => {
+            game.update(1 / 60.0);
+            if (game.players.length == 0 || game.time > 1000) {
+                this.games.delete(key);
+            }
+        });
+        this.expected += this.updateInterval;
+        setTimeout(()=>{this.updateGames()}, Math.max(0, this.updateInterval - dt));
     }
 
     private handlePlayerEvent(dataObject: string, player: ServerPlayer) {
